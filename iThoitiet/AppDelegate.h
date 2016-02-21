9e8775f691729dfabce39d94d33583c0837c44ff
Minh Khang <minhkhang@minhs-macbook-pro.local>
2016-02-21 08:35:44 +0700

    Initial Commit

---
+++ iThoitiet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iThoitiet
+//
+//  Created by Minh Khang on 2/21/16.
+//  Copyright © 2016 Minh Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iThoitiet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iThoitiet
+//
+//  Created by Minh Khang on 2/21/16.
+//  Copyright © 2016 Minh Khang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
